8db729edcb3fd2801739227b5d1b497f708192e4
Mohamed Kandeel <mohamedkandeel@intouch.com>
2024-05-09 01:24:15 +0300

    adjustments

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 // const connectDB = async ()=>{
-mongoose.connect("mongodb://mongodb:27017/e-commerce");
+
+// mongoose.connect("mongodb://mongodb:27017/e-commerce");
+mongoose.connect(process.env.MONGODB_URI);
 //     const product = mongoose.model('product', productSchema);
 //     const data = await product.find();
 //     console.warn(data);
